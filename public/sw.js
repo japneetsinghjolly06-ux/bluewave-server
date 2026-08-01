6cc9e0bc93e28ee3f6d9328177d35bbfe69cffb1
japneetsinghjolly06-ux <japneetsinghjolly06@gmail.com>
2026-08-01 14:33:01 +0530

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'bluewave-v102';
+const CACHE = 'bluewave-v103';
 const SHELL = ['/', '/manifest.webmanifest', '/icons/icon-192.png', '/icons/icon-512.png'];
 
 self.addEventListener('install', e => {
